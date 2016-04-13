83c5b9c3233a1b949b6c200b089381138d00285f
Simon Mihevc <simon.mihevc@gmail.com>
2016-04-13 22:08:16 +0200

    some clojure magic

--- src/mazicus/core.clj
+++ src/mazicus/core.clj
@@ -1,6 +1,30 @@
 (ns mazicus.core
   (:gen-class))
 
+(deftype Cell [x,y])
+
+
+(defn innerRow [size] (reduce conj (map #(hash-map % :noConnection) (range size))))
+
+(defn connection [size]
+  (reduce conj (map #(hash-map % (innerRow size)) (range size)))
+)
+
+(defn updateConnectionRow [row x value]
+  (conj row {x value})
+)
+
+(defn updateConnection [connection x y value]
+  (conj 
+    (reduce hash-map (reduce conj (filter (fn [[k v]] (not= k y)) connection)))
+    (reduce hash-map (
+                      updateConnectionRow (reduce conj (filter (fn [[k v]] (= k y)) connection)) x value
+                     )
+    )
+  )
+)
+
 (defn -main
   [& args]
-  (println "Amazing!"))
+  (print (connection 5))
+  )
