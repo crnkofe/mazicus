10e6313d44aa1d244f23a6ac90ab9c70be9b8fd3
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-13 14:28:04 +0200

    removed old code

--- src/simple.clj
+++ src/simple.clj
@@ -29,57 +29,16 @@
   [idx (reduce merge (map #(carve_binary_cell % (get row %) size) (keys row)))]
 )
 
-(defn carve_binary_wall_optimized [graph, size]
+(defn carve_binary_wall [graph, size]
   (into {} (map #(carve_binary_row % (get graph %) size) (keys graph)))
 )
 
-(comment "this code doesn't scale well and produces a stack overflow")
-(defn carve_binary_wall [from_list, visited, graph, size]
-  (if (empty? from_list)
-    (let [shuffle_from (shuffle from_list)
-          rnd_from (first shuffle_from)
-          neighbours (generate_ne (:x rnd_from) (:y rnd_from) size)
-          ]
-      (if (not (empty? neighbours))
-        (let [ne_neighbour (rand-nth neighbours)
-              filtered_from (filter #((and (= (:x rnd_from) (:x %)) (= (:y rnd_from) (:y %)))) from_list)
-              existing_neighbours (get-in graph [(:y rnd_from) (:x rnd_from) :neighbours])
-              updated_graph (assoc-in graph [(:y rnd_from) (:x rnd_from) :neighbours] (into [] (conj existing_neighbours ne_neighbour)))]
-          (carve_binary_wall (rest shuffle_from) (conj visited rnd_from) updated_graph size)
-        )
-        (carve_binary_wall (rest shuffle_from) (conj visited rnd_from) graph size)
-      )
-    )
-    graph
-  )
-)
-
-(defn generate_subindices [idx, size] 
-  (into [] (reduce concat (map #(vector [idx %]) (range size))))
-)
-
-(defn generate_indices [size]
-  (into [] (reduce concat (map #(generate_subindices % size) (range size))))
-)
-
 (defn carve_binary_maze [size]
-  (let [maze (grid size)
-        from (generate_indices size)
-        ]
-    (carve_binary_wall (map #(point (get % 0) (get % 1) maze) from) #{} maze size)
-  )
-)
-
-(defn carve_binary_maze_optimized [size]
   (let [maze (grid size)]
-    (carve_binary_wall_optimized maze size)
+    (carve_binary_wall maze size)
   )
 )
 
 (defn str_binary_maze [size]
   (print_grid (carve_binary_maze size))
-)
-
-(defn str_binary_maze_optimized [size]
-  (print_grid (carve_binary_maze_optimized size))
 )
