19583007cad89a038c9bfd60d90bc16f9d8e4cfe
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-14 16:54:33 +0200

    sidewinder algorithm booyah

--- src/sidewinder.clj
+++ src/sidewinder.clj
@@ -2,13 +2,14 @@
 
 (use 'common)
 (use 'clojure.pprint)
+(use '[clojure.set :only (difference)])
 
 (defn sidewinder_unused [graph, visited]
   (get-in graph [0 0])
 )
 
 (defn carve_top [cell graph]
-  (if (is_valid_grid_cell (:x cell) (:y cell) graph)
+  (if (is_valid_grid_cell (:x cell) (inc (:y cell)) graph)
     (assoc-in graph [(:y cell) (:x cell) :neighbours] (conj (get-in graph [(:y cell) (:x cell) :neighbours]) [(:x cell) (inc (:y cell))]))
     graph
   )
@@ -24,14 +25,16 @@
 
 (defn sidewinder_next [path, graph, visited]
   (let [last_node (first (reverse path))
-        coin_flip (rand-nth [:carve_east :join_path])]
-    (if (= coin_flip :carve_east)
+        coin_flip (rand-nth [:carve_east :join_path])
+        north_side (not (is_valid_grid_cell (:x last_node) (inc (:y last_node)) graph))
+        right_visited (> 0 (count (filter #(and (= (:x %) (inc (:x last_node))) (= (:y %) (:y last_node))) visited)))]
+    (if (or north_side (and (not right_visited) (= coin_flip :carve_east)))
       (if (is_valid_grid_cell (inc (:x last_node)) (:y last_node) graph)
         (let []
          [(conj path (get-in graph [(:y last_node) (inc (:x last_node))]))
           (carve_right last_node graph)]
-          )
-         [path (carve_top last_node graph)]
+        )
+        [path (carve_top last_node graph)]
       )
       (let [sidewinder_cell (rand-nth path)]
          [path (carve_top sidewinder_cell graph)]
@@ -39,6 +42,17 @@
     )
   )
 )
+
+(defn sidewinder_path [path, graph, visited]
+  (let [updated_visited (into [] (distinct (concat visited path)))
+        all_indices (map #(vector (:x %) (:y %)) (reduce concat (map vals (vals graph))))
+        differ (into [] (difference (into #{} all_indices) (into #{} (map #(vector (:x %) (:y %)) updated_visited))))]
+    (if (empty? differ)
+      [nil updated_visited]
+      [(first (sort-by #(+ (get % 0) (* 10 (get % 1))) differ)) updated_visited]
+    )
+  )
+)  
 
 (defn generate_sidewinder_path [size]
   (let [initial_maze (grid size)
@@ -49,8 +63,14 @@
            current_visited [initial_cell]]
       (let [[path maze] (sidewinder_next current_path updated_maze current_visited)]
         (if-not (= (count current_path) (count path))
-          (recur path maze current_path)
-          maze
+          (recur path maze current_visited)
+          (let [[path_start new_visited] (sidewinder_path path maze current_visited)]
+            (print_grid maze)
+            (if (= nil path_start)
+              maze
+              (recur [(get-in maze [(get path_start 1) (get path_start 0)])] maze new_visited)
+            )
+          )
         )
       )
     )
