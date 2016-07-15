6e82cae097e5a0ff5c9aff899fde4e10512db032
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-15 18:39:43 +0200

    added basic grid display

--- src/mazicus/core.clj
+++ src/mazicus/core.clj
@@ -11,23 +11,28 @@
 (use 'sidewinder)
 (use '[clojure.string :only (join)])
 
+(declare draw_maze)
+
 (def algorithms #{:binary :sidewinder})
 
-(defn draw []
+(defn draw [maze]
   (q/background 255)
+  (draw_maze maze)
+  (comment
   (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
    (doseq [t (range 0 100 0.01)]
      (q/point (* t (q/sin t))
               (* t (q/cos t))))))
+  )
 
-(defn setup []
+(defn setup [maze]
   (q/frame-rate 1)
   (q/background 200)
 )
 
 (def cli-options
   [["-a" "--algorithm NAME" "Algorithm name"
-      :default "binary"
+      :default :binary
       :parse-fn #(keyword %)
       :validate [#(contains? algorithms %) (str "Must be one of given names: " (join ", " (map name algorithms)))]]
    ["-s" "--size SIZE" "Maze size"
@@ -41,7 +46,44 @@
   (case algorithm
     :binary (carve_bin_alg_maze size)
     :sidewinder (generate_sidewinder_path size)
-    :default (carve_bin_alg_maze size)
+    :default (carve_bin_alg_maze size))
+)
+
+(defn neighbour [cell, direction]
+  (case direction
+    :left [(dec (:x cell)) (:y cell)]
+    :right [(inc (:x cell)) (:y cell)]
+    :top [(:x cell) (inc (:y cell))]
+    :bottom [(:x cell) (dec (:y cell))]
+  )
+)
+
+(defn draw_maze_cell [cell, x, y, cell_size]
+  (let [left (neighbour cell :left)
+        right (neighbour cell :right)
+        top (neighbour cell :top)
+        bottom (neighbour cell :bottom)]
+    (q/rect x y (:x cell_size) (:y cell_size))
+  )
+)
+
+(defn draw_maze_row [row, row_y, cell_size]
+  (doall (map #(draw_maze_cell % (* (:x %) (:x cell_size)) row_y cell_size) (map #(get row %) (sort (keys row)))))
+)
+
+(defn draw_maze [maze]
+  (let [row_keys (reverse (sort (keys maze)))
+        cell_size {:x 40 :y 40}
+        initial_row_y 0]
+    (loop [current_key (first row_keys)
+           row_keys (into [] (rest row_keys))
+           row_y initial_row_y]
+      (draw_maze_row (get maze current_key) row_y cell_size)
+      (if (not (empty? row_keys))
+        (recur (first row_keys) (rest row_keys) (+ row_y (:y cell_size)))
+        nil
+      )
+    )
   )
 )
 
@@ -50,6 +92,11 @@
         algorithm (get-in opts [:options :algorithm])
         size (get-in opts [:options :size])
         maze (generate_maze algorithm size)]
-    (q/sketch :title "Mazicus!" :setup setup :draw draw)
+    (q/sketch 
+      :title "Mazicus!" 
+      :setup (partial setup maze)
+      :draw (partial draw maze)
+      :size [800 600]
+    )
   )
 )
