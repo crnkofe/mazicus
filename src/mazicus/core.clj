838c1fc43d64a7630bef1a91ff0c579ddda73b2c
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-15 12:49:01 +0200

    added generating maze based on cmd line args

--- src/mazicus/core.clj
+++ src/mazicus/core.clj
@@ -9,6 +9,9 @@
 (use 'simple)
 (use 'binmaze)
 (use 'sidewinder)
+(use '[clojure.string :only (join)])
+
+(def algorithms #{:binary :sidewinder})
 
 (defn draw []
   (q/background 255)
@@ -22,6 +25,31 @@
   (q/background 200)
 )
 
+(def cli-options
+  [["-a" "--algorithm NAME" "Algorithm name"
+      :default "binary"
+      :parse-fn #(keyword %)
+      :validate [#(contains? algorithms %) (str "Must be one of given names: " (join ", " (map name algorithms)))]]
+   ["-s" "--size SIZE" "Maze size"
+      :default 10
+      :parse-fn #(Integer/parseInt %)
+      :validate [#(< % 1000) "Must be a number between 0 and 100"]]
+   ["-h" "--help"]])
+
+
+(defn generate_maze [algorithm, size]
+  (case algorithm
+    :binary (carve_bin_alg_maze size)
+    :sidewinder (generate_sidewinder_path size)
+    :default (carve_bin_alg_maze size)
+  )
+)
+
 (defn -main [& args] 
-  (q/sketch :title "Mazicus!" :setup setup :draw draw)
+  (let [opts (parse-opts args cli-options)
+        algorithm (get-in opts [:options :algorithm])
+        size (get-in opts [:options :size])
+        maze (generate_maze algorithm size)]
+    (q/sketch :title "Mazicus!" :setup setup :draw draw)
+  )
 )
