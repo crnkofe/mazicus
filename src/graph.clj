7d644c8efa790f1f3e1598030ef636e497c11e50
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-18 19:09:25 +0200

    fixed a bug with dijkstra

--- src/graph.clj
+++ src/graph.clj
@@ -1,16 +1,25 @@
 (ns graph)
 
 (use 'common)
-(use '[clojure.set :only (difference)])
+(use '[clojure.set :only (difference, union)])
 
+
+(defn contains_node [node, neighbours]
+  (some #(= % [(:x node) (:y node)]) neighbours)
+)
+
+(defn actual_neighbours [node, maze]
+  (let [linked_neighbours (map #(point (get % 0) (get % 1) maze) (:neighbours node))
+        reverse_linked_neighbours (filter #(contains_node node (:neighbours %)) (reduce concat (map vals (vals maze))))]
+    (into [] (union (into #{} linked_neighbours) (into #{} reverse_linked_neighbours)))
+  )
+)
 
 (defn dijkstra_update_distance [distances, from, node]
   (if (contains? distances node)
-    (if (< (get distances node) (get distances from 0))
-      (let [updated_from (assoc distances from (get distances node))]
-        (assoc updated_from node (+ (get distances from 0) 1))
-      )
+    (if (< (+ (get distances from 0) 1) (get distances node))
       (assoc distances node (+ (get distances from 0) 1))
+      distances
     )
     (assoc distances node (+ (get distances from 0) 1))
   )
@@ -21,7 +30,7 @@
     (loop [node (first nodes)
            remaining (rest nodes)
            current_distances distances]
-      (let [updated_distances (dijkstra_update_distance distances from node)]
+      (let [updated_distances (dijkstra_update_distance current_distances from node)]
         (if (not (empty? remaining))
           (recur (first remaining) (rest remaining) updated_distances)
           updated_distances
@@ -40,15 +49,12 @@
            visited #{}
            distances {start_node 0}]
       (if (> (count unvisited) 0)
-        (let [neighbours (into [] (difference (into #{} (map #(point (get % 0) (get % 1) maze) (:neighbours node))) visited))
+        (let [neighbours (into [] (difference (into #{} (actual_neighbours node maze)) visited))
               updated_distances (dijkstra_update_distances distances node neighbours)
-              new_unvisited (filter #(not= % node) (conj unvisited neighbours))]
+              new_unvisited (filter #(not= % node) (concat unvisited neighbours))]
           (if (empty? new_unvisited)
             (let [remaining (into [] (difference (difference (into #{} all_nodes) (into #{} new_unvisited)) visited))]
-              (if (empty? remaining)
-                updated_distances
-                (recur (first remaining) [(first remaining)] (conj visited node) updated_distances)
-              )
+              updated_distances
             )
             (recur (first new_unvisited) new_unvisited (conj visited node) updated_distances)
           )
