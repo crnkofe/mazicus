c56831fc5837ec1166919b68c87de7a6ebafc383
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-18 17:42:34 +0200

    dijkstra fixes

--- src/graph.clj
+++ src/graph.clj
@@ -1,16 +1,18 @@
 (ns graph)
 
 (use 'common)
+(use '[clojure.set :only (difference)])
+
 
 (defn dijkstra_update_distance [distances, from, node]
   (if (contains? distances node)
-    (if (< (get distances node) (get distances from))
+    (if (< (get distances node) (get distances from 0))
       (let [updated_from (assoc distances from (get distances node))]
-        (assoc updated_from node (+ (get distances from) 1))
+        (assoc updated_from node (+ (get distances from 0) 1))
       )
-      (assoc distances node (+ (get distances from) 1))
+      (assoc distances node (+ (get distances from 0) 1))
     )
-    (assoc distances node (+ (get distances from) 1))
+    (assoc distances node (+ (get distances from 0) 1))
   )
 )
 
@@ -35,12 +37,21 @@
         start_node (rand-nth all_nodes)]
     (loop [node start_node
            unvisited [start_node]
+           visited #{}
            distances {start_node 0}]
       (if (> (count unvisited) 0)
-        (let [neighbours (map #(point (get % 0) (get % 1) maze) (:neighbours node))
+        (let [neighbours (into [] (difference (into #{} (map #(point (get % 0) (get % 1) maze) (:neighbours node))) visited))
               updated_distances (dijkstra_update_distances distances node neighbours)
               new_unvisited (filter #(not= % node) (conj unvisited neighbours))]
-          (recur (first new_unvisited) new_unvisited updated_distances)
+          (if (empty? new_unvisited)
+            (let [remaining (into [] (difference (difference (into #{} all_nodes) (into #{} new_unvisited)) visited))]
+              (if (empty? remaining)
+                updated_distances
+                (recur (first remaining) [(first remaining)] (conj visited node) updated_distances)
+              )
+            )
+            (recur (first new_unvisited) new_unvisited (conj visited node) updated_distances)
+          )
         )
         distances
       )
