bfffaf5120575af1212b5dcb321264245afc84f0
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-20 14:09:47 +0200

    partially fixed sidewinder

--- src/sidewinder.clj
+++ src/sidewinder.clj
@@ -4,19 +4,17 @@
 (use 'clojure.pprint)
 (use '[clojure.set :only (difference)])
 
-(defn sidewinder_unused [graph, visited]
-  (get-in graph [0 0])
-)
-
 (defn carve_top [cell graph]
-  (if (is_valid_grid_cell (:x cell) (inc (:y cell)) graph)
-    (assoc-in graph [(:y cell) (:x cell) :neighbours] (conj (get-in graph [(:y cell) (:x cell) :neighbours]) [(:x cell) (inc (:y cell))]))
+  (if (is_valid_cell [(:x cell) (inc (:y cell))] (:size graph))
+    (connect_nodes cell (point [(:x cell) (inc (:y cell))] graph) graph)
     graph
   )
 )
 
 (defn carve_right [cell graph]
-  (assoc-in graph [(:y cell) (:x cell) :neighbours] (conj (get-in graph [(:y cell) (:x cell) :neighbours]) [(inc (:x cell)) (:y cell)]))
+  (if (is_valid_cell [(inc (:x cell)) (:y cell)] (:size graph))
+    (connect_nodes cell (point [(inc (:x cell)) (:y cell)] graph) graph)
+  )
 )
 
 (defn sidewinder_close [cell, graph, visited]
@@ -26,12 +24,12 @@
 (defn sidewinder_next [path, graph, visited]
   (let [last_node (first (reverse path))
         coin_flip (rand-nth [:carve_east :join_path])
-        north_side (not (is_valid_grid_cell (:x last_node) (inc (:y last_node)) graph))
+        north_side (not (is_valid_cell (:x last_node) (inc (:y last_node)) (:size graph)))
         right_visited (> 0 (count (filter #(and (= (:x %) (inc (:x last_node))) (= (:y %) (:y last_node))) visited)))]
     (if (or north_side (and (not right_visited) (= coin_flip :carve_east)))
-      (if (is_valid_grid_cell (inc (:x last_node)) (:y last_node) graph)
+      (if (is_valid_cell (inc (:x last_node)) (:y last_node) (:size graph))
         (let []
-         [(conj path (get-in graph [(:y last_node) (inc (:x last_node))]))
+         [(conj path (point [(:y last_node) (inc (:x last_node))] graph))
           (carve_right last_node graph)]
         )
         [path (carve_top last_node graph)]
@@ -45,8 +43,8 @@
 
 (defn sidewinder_path [path, graph, visited]
   (let [updated_visited (into [] (distinct (concat visited path)))
-        all_indices (map #(vector (:x %) (:y %)) (reduce concat (map vals (vals graph))))
-        differ (into [] (difference (into #{} all_indices) (into #{} (map #(vector (:x %) (:y %)) updated_visited))))]
+        all_indices (map coords (all_nodes graph))
+        differ (into [] (difference (into #{} all_indices) (into #{} (map coords updated_visited))))]
     (if (empty? differ)
       [nil updated_visited]
       [(first (sort-by #(+ (get % 0) (* 10 (get % 1))) differ)) updated_visited]
@@ -56,7 +54,7 @@
 
 (defn generate_sidewinder_path [size]
   (let [initial_maze (grid size)
-        initial_cell (get-in initial_maze [0 0])
+        initial_cell (point [0 0] initial_maze)
         initial_path [initial_cell]]
     (loop [current_path initial_path
            updated_maze initial_maze
@@ -65,10 +63,9 @@
         (if-not (= (count current_path) (count path))
           (recur path maze current_visited)
           (let [[path_start new_visited] (sidewinder_path path maze current_visited)]
-            (print_grid maze)
             (if (= nil path_start)
               maze
-              (recur [(get-in maze [(get path_start 1) (get path_start 0)])] maze new_visited)
+              (recur [(point path_start maze)] maze new_visited)
             )
           )
         )
