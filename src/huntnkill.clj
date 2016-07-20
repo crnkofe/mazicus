d060a98bb41fc7c5db228fbb12b78678e0f6af14
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-20 17:11:49 +0200

    removed some set conversions from hunt'n'kill

--- src/huntnkill.clj
+++ src/huntnkill.clj
@@ -5,7 +5,7 @@
 
 (defn filter_w_visited [index_node, maze, visited]
   (let [neighbours (generate_neighbours index_node (:size maze))
-        visited_neighbours (into [] (intersection (into #{} neighbours) (into #{} visited)))]
+        visited_neighbours (into [] (intersection (into #{} neighbours) visited))]
     (> (count visited_neighbours) 0)
   )
 )
@@ -16,7 +16,7 @@
       (if (empty? current_keys)
         nil
         (let [row_indices (into [] (map #(vector % (first current_keys)) (range (:size maze))))
-              non_visited_row_indices (into [] (difference (into #{} row_indices) (into #{} visited)))
+              non_visited_row_indices (into [] (difference (into #{} row_indices) visited))
               filtered_visited (filter #(filter_w_visited % maze visited) non_visited_row_indices)]
           (if (not (empty? filtered_visited))
             (first filtered_visited)
@@ -29,7 +29,7 @@
 )
 
 (defn valid_neighbours [maze, from, visited]
-  (into [] (difference (into #{} (generate_neighbours (coords from) (:size maze))) (into #{} visited)))
+  (into [] (difference (into #{} (generate_neighbours (coords from) (:size maze))) visited))
 )
 
 (defn carve_huntnkill_maze [size]
@@ -37,7 +37,7 @@
         initial_cell (point [(rand-int size) (rand-int size)]initial_maze)]
     (loop [current_cell initial_cell
            updated_maze initial_maze
-           current_visited [(coords initial_cell)]]
+           current_visited #{(coords initial_cell)}]
       (if-not (= (count current_visited) (* size size))
         (let [unvisited_neighbour_coords (valid_neighbours updated_maze current_cell current_visited)]
           (if (not (empty? unvisited_neighbour_coords))
@@ -49,7 +49,7 @@
                   hunted (point hunted_coords updated_maze)]
               (if (not (= hunted nil))
                 (let [neighbours (generate_neighbours (coords hunted) size)
-                      visited_neighbours (into [] (intersection (into #{} neighbours) (into #{} current_visited)))
+                      visited_neighbours (into [] (intersection (into #{} neighbours) current_visited))
                       random_from_coords (rand-nth visited_neighbours)
                       random_from (point random_from_coords updated_maze)]
                     (recur hunted (connect_nodes random_from hunted updated_maze) (conj current_visited hunted_coords))
