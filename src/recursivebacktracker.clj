5b4bd05ae19e01150e062b6017708238623ab3a8
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-20 17:14:47 +0200

    changed visited to set

--- src/recursivebacktracker.clj
+++ src/recursivebacktracker.clj
@@ -19,12 +19,12 @@
   (let [initial_maze (grid size)
         initial_cell (point [(rand-int size) (rand-int size)] initial_maze)]
     (loop [path_stack [initial_cell]
-           visited [(coords initial_cell)]
+           visited #{(coords initial_cell)}
            maze initial_maze]
       (if (empty? path_stack)
         maze
         (let [neighbours (generate_neighbours (coords (last path_stack)) size)
-              unvisited (into [] (difference (difference (into #{} neighbours) (into #{} (map coords path_stack))) (into #{} visited)))]
+              unvisited (into [] (difference (difference (into #{} neighbours) (into #{} (map coords path_stack))) visited))]
             (if (empty? unvisited)
               (let [updated_maze (carve_backtrack path_stack maze)
                     last_visited (last path_stack)]
