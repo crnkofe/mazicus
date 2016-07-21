ab13dcafca14098659a843c3f23b40f6bdb4e84d
Simon Mihevc <simon.mihevc@gmail.com>
2016-07-21 15:15:15 +0200

    added basic polar grid(untested)

--- src/common.clj
+++ src/common.clj
@@ -5,15 +5,39 @@
 (use '[clojure.set :only (difference, union)])
 
 (declare point)
+(declare is_valid_cell)
 (declare generate_neighbours)
+
+(defprotocol CellProtocol
+  (coords [node])
+)
 
 (defprotocol GridProtocol
   (random_point [grid]) 
 )
 
-(defrecord Cell [x, y, neighbours, valid_neighbours])
+(defrecord Cell [x, y, neighbours, valid_neighbours]
+  CellProtocol
+  (coords [node]
+    [(:x node) (:y node)]
+  )
+)
 
 (defrecord Grid [cells, size, all_indices, count]
+  GridProtocol
+  (random_point [grid]
+    (point (rand-nth (into [] (:all_indices grid))) grid)
+  )
+)
+
+(defrecord PolarCell [rad, dist, neighbours, valid_neighbours]
+  CellProtocol
+  (coords [node]
+    [(:x node) (:y node)]
+  )
+)
+
+(defrecord PolarGrid [cells, size, all_indices, count]
   GridProtocol
   (random_point [grid]
     (point (rand-nth (into [] (:all_indices grid))) grid)
@@ -25,13 +49,16 @@
   ([point, graph] (get (get (:cells graph) (get point 1)) (get point 0)))
 )
 
-(defn generate_row [idx, size] 
-  (
-   into 
-   (sorted-map) 
-   (
-    reduce conj (map #(hash-map % (->Cell % idx (generate_neighbours % idx size))) (range size))
-   )
+
+(defn generate_neighbours [coords, size] 
+  (let [x (get coords 0)
+        y (get coords 1)]
+    (filter 
+      #(is_valid_cell (get % 0) (get % 1) size) 
+      [
+        [(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]
+      ]
+    )
   )
 )
 
@@ -48,25 +75,24 @@
   )
 )
 
+(defn is_valid_polar_cell 
+  ([coords, size] (is_valid_polar_cell (get coords 0) (get coords 1) size))
+  ([angle, distance, size] (
+      cond 
+        (< angle 0) false
+        (>= angle 360) false
+        (< size 0) false
+        (>= size size) false
+        :else true
+    )
+  )
+)
+
 (defn is_valid_grid_cell [x, y, grid]
   (let [found (get-in grid [y x])]
     (if (= nil found)
       false
       true
-    )
-  )
-)
-
-(defn generate_neighbours 
-  ([coords, size] 
-   (generate_neighbours (get coords 0) (get coords 1) size)
-  )
-  ([x, y, size] 
-    (filter 
-      #(is_valid_cell (get % 0) (get % 1) size) 
-      [
-        [(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]
-      ]
     )
   )
 )
@@ -79,17 +105,13 @@
   (some #(= % [(:x node) (:y node)]) neighbours)
 )
 
-(defn coords [node]
-  [(:x node) (:y node)]
-)
-
 (defn all_maze_cells [maze]
   (reduce concat (map vals (vals (:cells maze))))
 )
 
 (defn connected_neighbours [node, maze]
   (let [linked_neighbours (map #(point % maze) (:neighbours node))
-        neighbours (map #(point % maze) (generate_neighbours (coords node) (:size maze)))
+        neighbours (map #(point % maze) (:valid_neighbours node))
         reverse_linked_neighbours (filter #(contains_node node (:neighbours %)) neighbours)]
     (into [] (union (into #{} linked_neighbours) (into #{} reverse_linked_neighbours)))
   )
@@ -103,16 +125,12 @@
   )
 )
 
-(defn connection [size]
-  (into (sorted-map) (reduce conj (map #(hash-map % (generate_row % size)) (range size))))
-)
-
 (defn generate_grid_row [idx, size] 
   (
    into 
    (sorted-map) 
    (
-    reduce conj (map #(hash-map % (->Cell % idx [] (generate_neighbours % idx size))) (range size))
+    reduce conj (map #(hash-map % (->Cell % idx [] (generate_neighbours [% idx] size))) (range size))
    )
   )
 )
@@ -155,6 +173,41 @@
     (->Grid 
       grid
       size 
+      indices
+      (count indices)
+    )
+  )
+)
+
+(defn generate_polar_neighbours [coords, size] 
+  (let [angle (get coords 0)
+        distance (get coords 1)
+        angular_distance (/ 360 size)]
+    (filter 
+      #(is_valid_polar_cell % size) 
+      [
+        [(- angle angular_distance) distance] [(+ angle angular_distance) distance] [angle (dec distance)] [angle (inc distance)]
+      ]
+    )
+  )
+)
+
+(defn generate_polar_grid_row [idx, size] 
+  (
+   into 
+   (sorted-map) 
+   (
+    reduce conj (map #(hash-map % (->PolarCell % idx [] (generate_polar_neighbours [% idx] size))) (range 0 360 (/ 360 size)))
+   )
+  )
+)
+
+(defn polar_grid[size]
+  (let [grid (into (sorted-map) (reduce conj (map #(hash-map % (generate_polar_grid_row % size)) (range size))))
+        indices (into #{} (map coords (reduce concat (map vals (vals grid)))))]
+    (->PolarGrid 
+      grid
+      size
       indices
       (count indices)
     )
